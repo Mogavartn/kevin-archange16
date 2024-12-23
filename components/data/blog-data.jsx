5487dec899c5801d60d6c764cf50262003e0eac5
Archange16 <asathoud16@gmail.com>
2024-12-23 19:17:42 +0100

    Modification partie blog

--- components/data/blog-data.jsx
+++ components/data/blog-data.jsx
@@ -1,10 +1,3 @@
-import image1 from "../../public/assets/img/blog/blog-1.jpg";
-import image2 from "../../public/assets/img/blog/blog-2.jpg";
-import image3 from "../../public/assets/img/blog/blog-3.jpg";
-import image4 from "../../public/assets/img/blog/blog-4.jpg";
-import image5 from "../../public/assets/img/blog/blog-5.jpg";
-import image6 from "../../public/assets/img/blog/blog-6.jpg";
-import image7 from "../../public/assets/img/blog/blog-7.jpg";
 import avatar1 from "../../public/assets/img/avatar/avatar-1.jpg";
 import avatar2 from "../../public/assets/img/avatar/avatar-2.jpg";
 import avatar3 from "../../public/assets/img/avatar/avatar-3.jpg";
@@ -13,41 +6,57 @@ import avatar5 from "../../public/assets/img/avatar/avatar-5.jpg";
 import avatar6 from "../../public/assets/img/avatar/avatar-6.jpg";
 import avatar7 from "../../public/assets/img/avatar/avatar-7.jpg";
 
-import image01 from "../../public/assets/img/blog/img_car/01.png";
-import image02 from "../../public/assets/img/blog/img_car/02.png";
-import image03 from "../../public/assets/img/blog/img_car/03.png";
-import image04 from "../../public/assets/img/blog/img_car/04.png";
-import image05 from "../../public/assets/img/blog/img_car/05.png";
+import image01 from "../../public/assets/img/blog/img_car/novembre23/01.png";
+import image02 from "../../public/assets/img/blog/img_car/novembre23/02.png";
+import image03 from "../../public/assets/img/blog/img_car/novembre23/03.png";
+import image04 from "../../public/assets/img/blog/img_car/novembre23/04.png";
 
-import image01000 from "../../public/assets/img/blog/img_car/01000.jpg";
-import image02000 from "../../public/assets/img/blog/img_car/02000.jpg";
-import image03000 from "../../public/assets/img/blog/img_car/03000.jpg";
-import image04000 from "../../public/assets/img/blog/img_car/04000.jpg";
-import image05000 from "../../public/assets/img/blog/img_car/05000.jpg";
+import image01000 from "../../public/assets/img/blog/img_car/novembre30/01.png";
+import image02000 from "../../public/assets/img/blog/img_car/novembre30/02.png";
+import image03000 from "../../public/assets/img/blog/img_car/novembre30/03.png";
+import image04000 from "../../public/assets/img/blog/img_car/novembre30/04.png";
+import image05000 from "../../public/assets/img/blog/img_car/novembre30/05.png";
+
+import image0100 from "../../public/assets/img/blog/img_car/decembre7/02.png";
+import image0200 from "../../public/assets/img/blog/img_car/decembre7/02.png";
+import image0300 from "../../public/assets/img/blog/img_car/decembre7/03.png";
+import image0400 from "../../public/assets/img/blog/img_car/decembre7/04.png";
+import image0500 from "../../public/assets/img/blog/img_car/decembre7/05.png";
+import image0600 from "../../public/assets/img/blog/img_car/decembre7/06.png";
+import image0700 from "../../public/assets/img/blog/img_car/decembre7/07.png";
+
+import image100 from "../../public/assets/img/blog/img_car/decembre21/01.png";
+import image200 from "../../public/assets/img/blog/img_car/decembre21/02.png";
+import image300 from "../../public/assets/img/blog/img_car/decembre21/03.png";
+import image400 from "../../public/assets/img/blog/img_car/decembre21/04.png";
+import image500 from "../../public/assets/img/blog/img_car/decembre21/05.png";
+import image600 from "../../public/assets/img/blog/img_car/decembre21/06.png";
+import image700 from "../../public/assets/img/blog/img_car/decembre21/07.png";
+
 
 const blogData = [
     {
         number: '01',
-        id: '1',
-        date: '19',
+        id: "1",
+        date: '30',
         month: 'Nov',
         year: '2024',
         comment: '2',
-        title: 'Software Development Agility a Primer',
+        title: "Comment lancer son projet d'entreprise sans se louper ? 🚀",
         image: image01,
-        images: [image02, image03, image04, image05], // Trois images pour cet élément
+        images: [image02, image03, image04], // Trois images pour cet élément
         avatar: avatar1,
         name: 'Derya Kurtulus',
         position: 'Heating Expert',
     },
     {
         number: '02',
-        id: 'ux-ui-designing-the-future-web-design',
-        date: '21',
+        id: "2",
+        date: '23',
         month: 'Nov',
         year: '2024',
         comment: '3',
-        title: 'UX/UI Designing the Future Web Design',
+        title: "Les 4 clés pour bâtir un empire financier d'après Père Riche Père Pauvre de Robert T. Kiyosaki !",
         image: image01000,
         images: [image02000, image03000, image04000, image05000], // Trois images pour cet élément
         avatar: avatar2,
@@ -56,101 +65,31 @@ const blogData = [
     },
     {
         number: '03',
-        id: 'pioneering-contactless-payment-technologies',
-        date: '23',
-        month: 'Nov',
+        id: "3",
+        date: '7',
+        month: 'dec',
         year: '2024',
         comment: '6',
-        title: 'Pioneering Contactless Payment Technologies',
-        image: image01000,
-        images: [image02000, image03000, image04000, image05000], // Trois images pour cet élément
+        title: "Les 4 principales causes d'échec de la création d'entreprise 🔑",
+        image: image0100,
+        images: [image0200, image0300, image0400, image0500, image0600, image0700], // Trois images pour cet élément
         avatar: avatar3,
         name: 'Lisa Thompson',
         position: 'Senior Engineer',
     },
     {
         number: '04',
-        id: 'balancing-ai-innovation-with-ethical-standards',
-        date: '26',
-        month: 'Nov',
+        id: "4",
+        date: '21',
+        month: 'dec',
         year: '2024',
         comment: '7',
-        title: 'Balancing AI Innovation with Ethical Standards',
-        image: image01000,
-        images: [image02000, image03000, image04000, image05000], // Trois images pour cet élément
+        title: "Devenir un chef d'entreprise accompli commence ici !",
+        image: image100,
+        images: [image200, image300, image400, image500, image600, image700], // Trois images pour cet élément
         avatar: avatar4,
         name: 'David Martinez',
         position: 'Senior Technician',
-    },
-    {
-        number: '05',
-        id: 'empowering-your-business-with-it-solutions',
-        date: '29',
-        month: 'Nov',
-        year: '2024',
-        comment: '5',
-        title: 'Empowering Your Business with IT Solutions',
-        image: image01000,
-        images: [image02000, image03000, image04000, image05000], // Trois images pour cet élément
-        avatar: avatar5,
-        name: 'Cansu Tuman',
-        position: 'Project Manager',
-    },
-    {
-        number: '06',
-        id: 'harnessing-the-power-of-technology',
-        date: '30',
-        month: 'Nov',
-        year: '2024',
-        comment: '3',
-        title: 'Harnessing the Power of Technology',
-        image: image01000,
-        images: [image02000, image03000, image04000, image05000], // Trois images pour cet élément
-        avatar: avatar6,
-        name: 'Devon Lane',
-        position: 'Technician',
-    },
-    {
-        number: '07',
-        id: 'cloud-computing-solutions-for-business',
-        date: '05',
-        month: 'Dec',
-        year: '2024',
-        comment: '7',
-        title: 'Cloud Computing Solutions for Business',        
-        image: image01000,
-        images: [image02000, image03000, image04000, image05000], // Trois images pour cet élément
-        avatar: avatar7,
-        name: 'Courtney Henry',
-        position: 'Sr. Developer',
-    },
-    {
-        number: '08',
-        id: 'cloud-computing-solutions-for-business',
-        date: '05',
-        month: 'Dec',
-        year: '2024',
-        comment: '7',
-        title: 'Cloud Computing Solutions for Business',        
-        image: image01000,
-        images: [image02000, image03000, image04000, image05000], // Trois images pour cet élément
-        avatar: avatar7,
-        name: 'Courtney Henry',
-        position: 'Sr. Developer',
-    },
-    {
-        number: '09',
-        id: 'cloud-computing-solutions-for-business',
-        date: '05',
-        month: 'Dec',
-        year: '2024',
-        comment: '7',
-        title: 'Cloud Computing Solutions for Business',        
-        image: image01000,
-        images: [image02000, image03000, image04000, image05000], // Trois images pour cet élément
-        avatar: avatar7,
-        name: 'Courtney Henry',
-        position: 'Sr. Developer',
     },
 ];
 
