f753e47581986ec02eabeef311dd0842bd8f4318
Archange16 <asathoud16@gmail.com>
2025-01-02 10:23:28 +0100

    le script quik

---
+++ components/pages/_document.js
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Ajoutez le script QuickReviewer ici */}
+          <script
+            src="//app.quickreviewer.com/proof/webproof/qrv2.js"
+            defer
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
