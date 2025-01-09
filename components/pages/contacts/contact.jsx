01755b9dea787895e4912111caba7075eaeb7cb2
Archange16 <asathoud16@gmail.com>
2025-01-09 09:15:22 +0100

    modifications du petit formulaire

--- components/pages/contacts/contact.jsx
+++ components/pages/contacts/contact.jsx
@@ -1,10 +1,7 @@
 "use client"
 import SEO from "@/components/data/seo";
 import HeaderOne from "@/components/layout/headers/header/header-one";
-import BreadCrumb from "../common/breadcrumb";
 import ScrollToTop from "../common/scroll/scroll-to-top";
-import FormArea from "./form";
-import FormAreaacc from "./formacc";
 import FooterThree from "@/components/layout/footers/footer-three";
 import Tab from "../home/tab/tab";
 
