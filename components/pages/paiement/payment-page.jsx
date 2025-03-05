13da37eec90a3216a62fcfe7982e1377ba382607
Archange16 <asathoud16@gmail.com>
2025-03-05 14:51:34 +0100

    vérification version mobile

--- components/pages/paiement/payment-page.jsx
+++ components/pages/paiement/payment-page.jsx
@@ -91,88 +91,95 @@ export default function PaymentPage() {
 
   return (
     <div className="section-padding container">
-      <div className="Formulaire_de_paiement">
-        <div className=" mr-md-10 formulaire-gauche">
-          <form>
-              <div className="row">
-                <div className="col-md-12 mb-3">
-                  <label htmlFor="name">Nom complet</label>
-                  <input id="name" type="text" placeholder="John Doe" required />
-                </div>
-              </div>
-              <div>
-                <div className="row">
-                  <div className="col-md-6 mb-3">
-                    <label htmlFor="email">E-mail</label>
-                    <input
-                      id="email"
-                      type="email"
-                      placeholder="john.doe@example.com"
-                      required
-                    />
+     <div class="container Formulaire_de_paiement">
+        <div class="row">
+            <div class="col-12 col-md-6"> 
+            <div className=" mr-md-10 formulaire-gauche">
+              <form>
+                  <div className="row">
+                    <div className="col-md-12 mb-3">
+                      <label htmlFor="name">Nom complet</label>
+                      <input id="name" type="text" placeholder="John Doe" required />
+                    </div>
                   </div>
-                  <div className="col-md-6 mb-3">
-                    <label htmlFor="countryCode">Téléphone</label>
-                    <input
-                      id="phone"
-                      type="tel"
-                      placeholder="+33612345678"
-                      required
-                    />
+                  <div>
+                    <div className="row">
+                      <div className="col-md-6 mb-3">
+                        <label htmlFor="email">E-mail</label>
+                        <input
+                          id="email"
+                          type="email"
+                          placeholder="john.doe@example.com"
+                          required
+                        />
+                      </div>
+                      <div className="col-md-6 mb-3">
+                        <label htmlFor="countryCode">Téléphone</label>
+                        <input
+                          id="phone"
+                          type="tel"
+                          placeholder="+33612345678"
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-6 mb-3">
+                        <label htmlFor="city">Ville</label>
+                        <input id="city" type="text" placeholder="Paris" required />
+                      </div>
+                      <div className="col-md-6 mb-3">
+                        <label htmlFor="region">Région</label>
+                        <input id="region" type="text" placeholder="Île-de-France" />
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-6 mb-3">
+                        <label htmlFor="streetLine1">Adresse ligne 1</label>
+                        <input
+                          id="streetLine1"
+                          type="text"
+                          placeholder="1 Rue de la Paix"
+                          required
+                        />
+                      </div>
+                      <div className="col-md-6 mb-3">
+                        <label htmlFor="postcode">Code postal</label>
+                        <input id="postcode" type="text" placeholder="75002" required />
+                      </div>
+                    </div>
                   </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-6 mb-3">
-                    <label htmlFor="city">Ville</label>
-                    <input id="city" type="text" placeholder="Paris" required />
-                  </div>
-                  <div className="col-md-6 mb-3">
-                    <label htmlFor="region">Région</label>
-                    <input id="region" type="text" placeholder="Île-de-France" />
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-6 mb-3">
-                    <label htmlFor="streetLine1">Adresse ligne 1</label>
-                    <input
-                      id="streetLine1"
-                      type="text"
-                      placeholder="1 Rue de la Paix"
-                      required
-                    />
-                  </div>
-                  <div className="col-md-6 mb-3">
-                    <label htmlFor="postcode">Code postal</label>
-                    <input id="postcode" type="text" placeholder="75002" required />
-                  </div>
-                </div>
-              </div>
-          </form>
-        </div>
-        <div className="pl-20 align-items-center formulaire-droit">
-          <div className="project-info mb-5">
-              <ul>
-                <li>Formation: <span>{orderResponse?.description}</span></li>
-                <li>Prix: <span className="value">{orderResponse?.amount} €</span></li>
-              </ul>
+              </form>
             </div>
-            <div className="row mt-1">
-              <div className="col-md-12 mb-1">
-                {/* Champ de carte Revolut (uniquement pour le mode de paiement par carte) */}
-                <div id="card-field"></div>
+            </div>
+            <div class="col-12 col-md-6"> 
+            <div className="pl-20 align-items-center formulaire-droit">
+                <div className="project-info mb-5">
+                    <ul>
+                      <li>Formation: <span>{orderResponse?.description}</span></li>
+                      <li>Prix: <span className="value">{orderResponse?.amount} €</span></li>
+                    </ul>
+                  </div>
+                  <div className="row mt-1">
+                    <div className="col-md-12 mb-1">
+                      {/* Champ de carte Revolut (uniquement pour le mode de paiement par carte) */}
+                      <div id="card-field"></div>
+                    </div>
+                    <button id="button-submit" type="button" /* onClick={handleSubmit} */ >
+                      Payer
+                    </button>
+                    <div className="mt-5">
+                      <img src={image1?.src} alt="Visa MasterCard" />
+                    </div>
+                    {/* Composant RevolutCardField (uniquement pour le mode de paiement par carte) */}
+                    {/* token && <RevolutCardField token={token} /> */}
+                    {token && <RevolutCardField token={token} onSubmit={handleSubmit} />}
+                  </div>
               </div>
-              <button id="button-submit" type="button" /* onClick={handleSubmit} */ >
-                Payer
-              </button>
-              <div className="mt-5">
-                <img src={image1?.src} alt="Visa MasterCard" />
-              </div>
-              {/* Composant RevolutCardField (uniquement pour le mode de paiement par carte) */}
-              {/* token && <RevolutCardField token={token} /> */}
-              {token && <RevolutCardField token={token} onSubmit={handleSubmit} />}
             </div>
         </div>
       </div>
+
     </div>
   );
 }
