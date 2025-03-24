8cb2fddd67c1346a2fab4ed9a303902459eaccfd
Archange16 <asathoud16@gmail.com>
2025-03-24 14:24:15 +0100

    modifications1

--- components/pages/portfolio/portfolio-details/portfolio-details.jsx
+++ components/pages/portfolio/portfolio-details/portfolio-details.jsx
@@ -2,16 +2,14 @@
 
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import testimonialBg from "../../../../public/assets/img/testimonial/testimonial.png";
 import Link from 'next/link';
-import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectFade, Autoplay, Navigation } from 'swiper/modules';
-import quoteIcon from "../../../../public/assets/img/testimonial/testimonial-quote.png";
 import Temoignage from '../../temoignage/temoignages';
 
 const PortfolioDetailsMain = ({ singleData }) => {
   const router = useRouter();
-  const [amount, setAmount] = useState(singleData?.formation?.prix || 69); // Montant initial
+  const [timeLeft, setTimeLeft] = useState(48 * 60 * 60); // 48h en secondes
+  const [amount, setAmount] = useState(99); // Montant initial
+  const [promoAmount, setpromoAmount] = useState( 69); // Montant initial
   const [currency, setCurrency] = useState('EUR'); // Devise par défaut
   const [isLoading, setIsLoading] = useState(false); // État de chargement
   const [error, setError] = useState(null); // Gestion des erreurs
@@ -25,39 +23,31 @@ const PortfolioDetailsMain = ({ singleData }) => {
   const handleCheckboxChange = (id) => {
     setSelectedPacks((prevState) => {
       const newState = { ...prevState, [id]: !prevState[id] };
-
-      // Calculer le nouveau montant en fonction des cases cochées
-      let newAmount = singleData?.formation?.prix || 69; // Montant initial
+      let newAmount = singleData?.formation?.prix || 99; // Montant initial
       const checkedPacksCount = Object.values(newState).filter((checked) => checked).length;
-
-      // Incrémenter de 10€ par case cochée
       newAmount += checkedPacksCount * 10;
-
       setAmount(newAmount); // Mettre à jour le montant
-
       return newState;
     });
   };
 
   // Fonction pour créer une commande
   const createOrder = async () => {
-    // Validation des données
     if (!amount || amount <= 0) {
       setError('Le montant de la commande est invalide.');
       return;
     }
-    setIsLoading(true); // Activer l'état de chargement
-    setError(null); // Réinitialiser les erreurs
+    setIsLoading(true); 
+    setError(null); 
 
     const orderData = {
-      amount: amount, // Montant de la commande
-      currency: currency, // Devise
+      amount,
+      currency,
       description: singleData.titre,
-      selectedPacks, // Ajouter les packs sélectionnés à la commande
+      selectedPacks,
     };
 
     try {
-      // Envoi de la requête POST à l'API
       const response = await fetch('/api/create-order', {
         method: 'POST',
         headers: {
@@ -70,143 +60,138 @@ const PortfolioDetailsMain = ({ singleData }) => {
 
       if (response.ok) {
         console.log('Commande créée avec succès:');
-
-        // Stocker la réponse de la commande dans localStorage
         localStorage.setItem("orderResponse", JSON.stringify(data));
-
-        // Rediriger vers la page de paiement
         router.push("/paiement");
       } else {
         console.error('Erreur lors de la création de la commande:', data.error);
         setError(data.error || 'Une erreur est survenue lors de la création de la commande.');
-        if (data.details) {
-          console.error('Détails de l\'erreur:', data.details);
-        }
       }
     } catch (error) {
       console.error('Erreur réseau ou autre:', error.message);
       setError('Une erreur réseau est survenue. Veuillez réessayer.');
     } finally {
-      setIsLoading(false); // Désactiver l'état de chargement
+      setIsLoading(false);
     }
+  };
+
+  // Utilisation de useEffect pour démarrer un timer qui décrémente chaque seconde
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => (prevTime > 0 ? prevTime - 1 : 0)); 
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  // Fonction pour formater le temps restant en heures:minutes:secondes
+  const formatTime = (seconds) => {
+    const h = String(Math.floor(seconds / 3600)).padStart(2, "0");
+    const m = String(Math.floor((seconds % 3600) / 60)).padStart(2, "0");
+    const s = String(seconds % 60).padStart(2, "0");
+    return `${h}:${m}:${s}`;
   };
 
   return (
     <div className="skill__two-tab-details-content mt-5 mb-5">
-      <div className="row justify-content-center gy-4 ">
+      <div className="row justify-content-center gy-4">
         <div className="col-xl-7">
-          <div className="">
-            <h2 className="text-center">{singleData?.titre}</h2>
-            <p className="mb-1 fw-bold fs-5">{singleData.description1}</p>
-            <p>{singleData.description}</p>
-          </div>
+          <h2 className="text-center">{singleData?.titre}</h2>
+          <p className="mb-1 fw-bold fs-5">{singleData.description1}</p>
+          <p>{singleData.description}</p>
+
           <div className="image-formation">
-            <img className="img__full rounded mt-5" src={singleData?.image?.src} alt={singleData?.id} />
+            <img
+              className="img__full rounded mt-5"
+              src={singleData?.image?.src}
+              alt={singleData?.id}
+            />
           </div>
         </div>
 
         <div className="col-xl-5 project-info-left">
           <div className="project-info">
             <div className="project-info-top">
-              <h4>{singleData?.titre}</h4>
+              <h4>
+                {['anglais-debutant-a1-a2', 'anglais-intermediaire-b1-b2', 'anglais-avance-c1-c2'].includes(singleData?.id) ? (
+                  <span className="text-danger">Offre Spéciale !</span>
+                ) : (
+                  singleData?.titre
+                )}
+              </h4>
             </div>
             <ul>
-              <li>niveau: <span> {singleData.niveau}</span></li>
-              <li>public: <span> {singleData.public}</span></li>
-              <li>duree: <span> {singleData.duree}</span></li>
-              <li>Prix: <span className="value"> {amount} €</span></li> {/* Affiche le prix dynamique */}
-              <li className="mt-5">
-                {/* Ajout des cases à cocher pour les packs */}
+              <li>niveau: <span>{singleData.niveau}</span></li>
+              <li>public: <span>{singleData.public}</span></li>
+              <li>duree: <span>{singleData.duree}</span></li>
+              <li>
+                Prix: <span className="value text-muted text-decoration-line-through">{amount} € </span>
                 {singleData?.id === 'anglais-debutant-a1-a2' && (
-                  <div class="table">
-                    <table class="styled-table">
-                    <thead>
-                      <tr>
-                        <th className="text-center">Économisez $10,00 soit 12 % de réduction</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                      <td>
-                        <label class="form-check-label" for="flexCheckDefault">
-                          <input
-                            type="checkbox"
-                            class="form-check-input"
-                            checked={selectedPacks['anglais-debutant-a1-a2']}
-                            onChange={() => handleCheckboxChange('anglais-debutant-a1-a2')}
-                          />
-                          En choisissant le Pack Anglais Débutant + Intermédiaire
-                        </label>
-                      </td>
-                      </tr>
-                      <tr>
-  
-                        <td>
-                            <label class="form-check-label" for="flexCheckDefault">
-                            <input
-                              type="checkbox"
-                              class="form-check-input"
-                              checked={selectedPacks['anglais-intermediaire-b1-b2']}
-                              onChange={() => handleCheckboxChange('anglais-intermediaire-b1-b2')}
-                            />
-                             En choisissant le Pack Anglais Débutant + Intermédiaire + Avancé
-                            </label>
-                        </td>
-                      </tr>
-                    </tbody>
-                    </table>
-                  </div>
+                  <span className="h3 text-success fw-bold">  { promoAmount} €</span>
                 )}
-
-                {singleData?.id === 'anglais-intermediaire-b1-b2' && (
-                  <div class="table">
-                     <table class="styled-table">
+              </li>
+              <li>
+                {singleData?.id === 'anglais-debutant-a1-a2' && (
+                  <p className="text-secondary fs-6">
+                    Dépêche-toi, offre limitée ! <span className="fs-6 text-danger font-monospace mb-4">⏳ {formatTime(timeLeft)} restantes</span>
+                  </p>
+                )}
+              </li>
+              <li className="mt-5">
+                {singleData?.id === 'anglais-debutant-a1-a2' && (
+                  <div className="table">
+                    <table className="styled-table">
                       <thead>
                         <tr>
                           <th className="text-center">Économisez $10,00 soit 12 % de réduction</th>
                         </tr>
                       </thead>
                       <tbody>
-                        <tr>
-                        <td>
-                          <label class="form-check-label" for="flexCheckDefault">
-                            <input
-                              type="checkbox"
-                              class="form-check-input"
-                              checked={selectedPacks['anglais-intermediaire-b1-b2']}
-                              onChange={() => handleCheckboxChange('anglais-intermediaire-b1-b2')}
-                            />
-                            En choisissant le Pack Anglais Intermédiaire + Avancé 
-                          </label>
-                        </td>
-                        </tr>
+                        {['anglais-debutant-a1-a2', 'anglais-intermediaire-b1-b2'].map((id) => (
+                          <tr key={id}>
+                            <td>
+                              <label className="form-check-label">
+                                <input
+                                  type="checkbox"
+                                  className="form-check-input"
+                                  checked={selectedPacks[id]}
+                                  onChange={() => handleCheckboxChange(id)}
+                                />
+                                En choisissant le Pack {id.replace('-', ' ')}
+                              </label>
+                            </td>
+                          </tr>
+                        ))}
                       </tbody>
-                     </table>
+                    </table>
                   </div>
                 )}
 
-                <button onClick={createOrder} className="btn-one mb-4" disabled={isLoading}>
+                <button onClick={createOrder} className="btn-one" disabled={isLoading}>
                   {isLoading ? 'Traitement...' : 'Acheter Maintenant 🔥'}
                 </button>
+
+                {selectedPacks['anglais-intermediaire-b1-b2'] && (
+                  <p className="text-primary-emphasis mt-3">
+                    Bonus : Inscrivez-vous aujourd'hui et recevez un guide gratuit des 100 phrases essentielles en anglais!
+                  </p>
+                )}
               </li>
-              {error && <li className="text-danger">{error}</li>} {/* Affichage des erreurs */}
+
+              {error && <li className="text-danger">{error}</li>}
             </ul>
+
             <div className="btn-achat-formation">
               <Temoignage className="temoignage" />
             </div>
           </div>
+
           <div className="project-info mt-20">
             <div className="project-info-top">
               <h4>Ce que vous allez maîtriser :</h4>
             </div>
             <ul>
-              <div>
-                <div>
-                  {singleData?.objectifs_pedagogiques?.map((objectif, index) => (
-                    <li key={index}><span>{objectif}</span></li>
-                  ))}
-                </div>
-              </div>
+              {singleData?.objectifs_pedagogiques?.map((objectif, index) => (
+                <li key={index}><span>{objectif}</span></li>
+              ))}
             </ul>
           </div>
         </div>
